c39199b9ae3be7453ab7ffb25f4def30f46d9c56
Leandro Rovagnoli <leandro.rovagnoli@gmail.com>
2021-01-05 19:26:02 +0000

    Criação de Model para gerenciamento de usuário/autenticação;

---
+++ backend/src/CsgoEssentials.WebAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsgoEssentials.WebAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(maximumLength: 60, ErrorMessage = "O campo {0} precisa ter no mínimo {2} e no máximo {1} caracteres.", MinimumLength = 4)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(maximumLength: 20, ErrorMessage = "O campo {0} precisa ter no mínimo {2} e no máximo {1} caracteres.", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
