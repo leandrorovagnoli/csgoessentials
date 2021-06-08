9c8e98198b80847a45c160e89db4c4f406a98a95
PauloSeph <paulo.seph@hotmail.com>
2021-06-08 19:14:09 -0300

    structuring frontend

---
+++ frontend/csgoEssentials/src/app/modules/AdminModule/module-admin.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './components/admin/admin.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AdminComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModuleAdminModule { }
