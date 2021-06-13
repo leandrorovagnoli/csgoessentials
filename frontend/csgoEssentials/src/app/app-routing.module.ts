17d6b418cd914d9c2c09c3028fed4bd4015b39d6
PauloSeph <paulo.seph@hotmail.com>
2021-06-13 00:39:48 -0300

    Updating home layout

--- frontend/csgoEssentials/src/app/app-routing.module.ts
+++ frontend/csgoEssentials/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './modules/Admin/components/admin/admin.component';
 import { HomeComponent } from './modules/Home/components/home.component';
+import { VideoComponent } from './modules/video/video.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent, children: [
+    {path: 'video', component: VideoComponent}
+  ]},
   {path: 'admin', component: AdminComponent},
+
 
 ];
 
--- frontend/csgoEssentials/src/app/app.module.ts
+++ frontend/csgoEssentials/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './modules/Admin/admin.module';
 import { HomeModule } from './modules/Home/home.module';
+import { FooterComponent } from './core/template/footer/footer.component';
+import { HeaderComponent } from './core/template/header/header.component';
+import { MainComponent } from './core/template/main/main.component';
+import { SidebarComponent } from './core/template/sidebar/sidebar.component';
+import { VideoComponent } from './modules/video/video.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    SidebarComponent,
+    VideoComponent,
   ],
   imports: [
     BrowserModule,
